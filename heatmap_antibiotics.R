4626acb4788d7815b5991961df8758ba232b7bfc
Hugo Castelán Sánchez <64658071+hugocastelan@users.noreply.github.com>
2024-10-26 12:32:44 -0400

    Update heatmap_antibiotics.R

--- heatmap_antibiotics.R
+++ heatmap_antibiotics.R
@@ -1,18 +1,18 @@
+# Cargar librerías
 library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(reshape2)
 
-# path of files 
+# Ruta de la carpeta con archivos
 folder_path <- "Downloads/resultadosantibioticoscarlos_bacteria/"
 
-# list of files .txt
+# Listado de archivos .txt
 file_list <- list.files(path = folder_path, pattern = "\\.txt$", full.names = TRUE)
 
-# Funtion to obtain the data 
+# Función para procesar los datos
 process_data <- function(file) {
-  
   data <- read_delim(file, delim = "\t") %>%
     select(Best_Hit_ARO, Best_Identities, Antibiotic)
   
@@ -21,15 +21,14 @@ process_data <- function(file) {
   data <- data %>%
     mutate(Cepa = cepa)
   
-  
   return(data)
 }
 
-# make a dataframe 
+# Crear un dataframe combinado con todos los archivos
 combined_data <- bind_rows(lapply(file_list, process_data))
 combined_data <- combined_data[!is.na(combined_data$Antibiotic), ]
 
-# make heatmap with ggplot2
+# Crear el heatmap
 ggplot(combined_data, aes(x = Antibiotic, y = Cepa, fill = Best_Identities)) +
   geom_tile(color = "white", size = 0.1) +
   scale_fill_gradient(low = "gray97", high = "#012a4a", name = "Best_Identities",
@@ -44,17 +43,23 @@ ggplot(combined_data, aes(x = Antibiotic, y = Cepa, fill = Best_Identities)) +
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
-
+# Crear la matriz de abundancia
 combined_data2 <- combined_data %>% 
-          mutate(Presence = ifelse(Best_Identities > 0, 1, 0)) %>%
-          select(Cepa, Antibiotic, Presence)
+  mutate(Presence = ifelse(Best_Identities > 0, 1, 0)) %>%
+  select(Cepa, Antibiotic, Presence)
 
+# Renombrar columnas
 colnames(combined_data2) <- c("Strain", "Antibiotic", "Presence")
 
+# Crear y ajustar la matriz de abundancia
 matriz <- dcast(combined_data2, Strain ~ Antibiotic, value.var = "Presence", fill = 0)
-
 matriz[, -1] <- apply(matriz[, -1], 2, function(x) ifelse(x > 1, 1, x))
 
+# Imprimir la matriz en consola
 print(matriz)
 
+# Guardar la matriz en un archivo CSV
 write.csv(matriz, "Downloads/resultadosantibioticoscarlos_bacteria/matriz_antibiotics.csv", row.names = FALSE)
+
+# Mensaje de confirmación
+print("Matriz de abundancia guardada en 'Downloads/resultadosantibioticoscarlos_bacteria/matriz_antibiotics.csv'")
